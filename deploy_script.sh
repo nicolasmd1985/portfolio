bab204f17a80ac545a4d7b8411f22d684d477db2
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2025-04-22 19:55:45 -0400

    update deploy

---
+++ deploy_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Change to the directory of the script
+cd "$(dirname "$0")"
+
+# Stop any running containers
+docker compose down
+
+# Pull the latest images
+docker compose pull
+
+# Run migrations
+docker compose run --rm web bundle exec rails db:migrate
+
+# Start the containers in detached mode
+docker compose up -d --remove-orphans
+
+# Clean up old images and volumes
+docker image prune -f
+docker volume prune -f 
