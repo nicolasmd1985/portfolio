6a66841d1773fcb144ac1a5d88a04350209bba01
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2025-05-26 14:08:17 -0400

    add i18 tanslate

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  connect() {
+    // Close dropdown when clicking outside
+    document.addEventListener("click", this.handleClickOutside.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleClickOutside.bind(this))
+  }
+
+  toggle(event) {
+    event.stopPropagation()
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  select() {
+    this.menuTarget.classList.add("hidden")
+  }
+
+  handleClickOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.add("hidden")
+    }
+  }
+} 
