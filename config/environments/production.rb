74465a6dd310ebcd7af998cdc491aac2c23e68f2
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2025-06-16 18:57:06 -0400

    fix domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,10 +83,10 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "nicolasmahecha.com",     # Allow requests from main domain
-    /.*\.nicolasmahecha\.com/ # Allow requests from subdomains
-  ]
+  # config.hosts = [
+  #   "nicolasmahecha.com",     # Allow requests from main domain
+  #   /.*\.nicolasmahecha\.com/ # Allow requests from subdomains
+  # ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
