7c56164a651b9192dc050d285ecfd4bbb77210df
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2025-06-16 18:23:16 -0400

    change send email account

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
     mail(
-      to: "nicolasmelectronic@gmail.com",
+      to: "ordenappweb@gmail.com",
       subject: "New Contact Form Submission: #{@contact.subject}"
     )
   end
