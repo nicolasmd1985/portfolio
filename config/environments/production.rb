4a4df8fe9154a34c7c693dbd036f7a23e9be31b6
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2025-06-16 19:05:06 -0400

    fix domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "nicolasmahecha.com" }
+  # config.action_mailer.default_url_options = { host: "nicolasmahecha.com" }
 
   # Configure email delivery
   config.action_mailer.delivery_method = :smtp
